c6138a06533738ea13892d5cd2b0fbda626dab49
James McGauran <20632552+jamesmagoo@users.noreply.github.com>
2024-08-13 17:46:18 +0100

    go fmt

--- api.go
+++ api.go
@@ -6,29 +6,36 @@ import (
 )
 
 type APIServer struct {
-    addr string
+	addr string
 }
 
 func NewAPIServer(addr string) *APIServer {
-    return &APIServer{
-        addr: addr,
-    }
+	return &APIServer{
+		addr: addr,
+	}
 }
 
 func (s *APIServer) Run() error {
 
-    router := http.NewServeMux()
-    router.HandleFunc("GET /users/{uid}", func(w http.ResponseWriter, r *http.Request) {
-        userID := r.PathValue("uid")
-        w.Write([]byte("User ID:" + userID))
-    })
+	router := http.NewServeMux()
+	router.HandleFunc("GET /users/{uid}", func(w http.ResponseWriter, r *http.Request) {
+		userID := r.PathValue("uid")
+		w.Write([]byte("User ID:" + userID))
+	})
 
-    server := http.Server{
-        Addr: s.addr,
-        Handler: router,
-    }
+	server := http.Server{
+		Addr:    s.addr,
+		Handler: RequestLoggerMiddleware(router),
+	}
 
-    log.Printf("Serving on PORT %v", s.addr)
+	log.Printf("Serving on PORT %v", s.addr)
 
-    return server.ListenAndServe()
+	return server.ListenAndServe()
+}
+
+func RequestLoggerMiddleware(next http.Handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("method %s, path: %s", r.Method, r.URL.Path)
+		next.ServeHTTP(w, r)
+	}
 }
